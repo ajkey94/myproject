1176916a193d2c2bbae7662d6cbb1da535dcdfbd
Alex Keyport <Alexkeyport94@gmail.com>
2024-12-18 16:36:33 -0600

    first commit

---
+++ mypackage.go
@@ -0,0 +1,15 @@
+// mypackage.go
+
+package mypackage
+
+import "fmt"
+
+// Greet is an example function that greets the user
+func Greet(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
+
+// Add is an example function that adds two numbers
+func Add(a, b int) int {
+	return a + b
+}
